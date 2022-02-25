1a62a8917212982c4df1774ae0d5b1cfbd4df4ff
Mona90 <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@github.com>
2022-02-26 01:34:01 +0200

    change emait validation code

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -14,13 +14,14 @@ const onSubmit = values =>{
     console.log("Form data", values)
 }
 const validate = values => {
+    const regex = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
     let errors = {}
     if(!values.name){
         errors.name = "required"
     }
     if(!values.email){
         errors.email = "required"
-    }else if(!/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(values.email)){
+    }else if(!regex.test(values.email)){
         errors.email = "Invalid email formate"
     }
     if(!values.mobile){
